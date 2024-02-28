7b83d4368297d193e8992094f826947eb0c0dc42
Kirek <kirek_priest@mail.ru>
2024-02-29 02:02:22 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import client.Animal;
+import client.Cat;
+import client.FlyingFish;
+import client.Owner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Cat nyaasu = new Cat("Мяут", 4, LocalDate.of(2022, 11, 27), new Owner());
+        Seel seal = new Seel("Сфил", 39, LocalDate.of(2023, 05, 14), new Owner() );
+        FlyingFish flyingFish = new FlyingFish("Шарпедо", 88, LocalDate.of(1975, 7, 18), new Owner());
+        System.out.println(nyaasu.getType());
+        System.out.println(nyaasu);
+        System.out.println("Навыки:");
+        nyaasu.skills();
+        System.out.println();
+        System.out.println(seal.getType());
+        System.out.println(seal);
+        System.out.println("Навыки:");
+        seal.skills();
+        System.out.println();
+        System.out.println(flyingFish.getType());
+        System.out.println(flyingFish);
+        System.out.println("Навыки:");
+        flyingFish.skills();
+
+    }
+}
